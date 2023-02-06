bce3103b5bf3f0e95d0277ca3d322bb3ecb2dc0a
Tayshawn Hill <tayshawnhill@Tayshawns-MacBook-Pro.local>
2023-02-05 18:01:57 -0800

    learning rust

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+// use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+//  mod crate_declarations;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+// macro_rules! hello {
+//     () => {
+//         println!("Hello, world! this is how macros work");
+//     };
+// }
+
+// fn main() {
+//     hello!();
+// }
